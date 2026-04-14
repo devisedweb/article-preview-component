c96d8e2efe266b41f41d3b053dccf48f10ff1355
DevisedWeb <contact@devisedweb.com>
2026-04-14 10:18:43 +0500

    Make share component disappear on mobile

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ const shareMobile = document.getElementById("share-mobile");
 
 const isMobile = window.matchMedia("(max-width: 767px)");
 
-shareBtn.addEventListener("click", function () {
+shareBtn.addEventListener("click", function (e) {
   if (isMobile.matches) {
     shareMobile.style.display = "block";
+  }
+  e.stopPropagation();
+});
+
+window.addEventListener("click", function (e) {
+  if (
+    shareMobile.style.display === "block" &&
+    !shareMobile.contains(e.target)
+  ) {
+    shareMobile.style.display = "none";
   }
 });
