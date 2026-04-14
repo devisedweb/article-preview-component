92db2cc87aaa444357a394a629deb9aa545eec1e
DevisedWeb <contact@devisedweb.com>
2026-04-14 09:51:41 +0500

    Make share component appear on click on mobile

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const shareBtn = document.getElementById("share-btn");
+const shareMobile = document.getElementById("share-mobile");
+
+const isMobile = window.matchMedia("(max-width: 767px)");
 
 shareBtn.addEventListener("click", function () {
-  console.log("You clicked me!");
+  if (isMobile.matches) {
+    shareMobile.style.display = "block";
+  }
 });
